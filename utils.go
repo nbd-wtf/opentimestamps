b6c735501dc58a3cd036c6cd82538c41dc0aeae8
fiatjaf <fiatjaf@gmail.com>
2023-09-27 08:59:15 -0300

    remove println calls.

--- utils.go
+++ utils.go
@@ -1,8 +1,6 @@
 package opentimestamps
 
 import (
-	"encoding/hex"
-	"fmt"
 	"io"
 	"strings"
 )
@@ -28,31 +26,25 @@ func NewBuffer(buf []byte) Buffer {
 }
 
 func (buf Buffer) readBytes(n int) ([]byte, error) {
-	fmt.Println("reading", n, "bytes")
-
+	// fmt.Println("reading", n, "bytes")
 	if *buf.pos >= len(buf.buf) {
 		return nil, io.EOF
 	}
 	res := buf.buf[*buf.pos : *buf.pos+n]
 	*buf.pos = *buf.pos + n
-	fmt.Println("->", hex.EncodeToString(res))
+	// fmt.Println("->", hex.EncodeToString(res))
 	return res, nil
 }
 
 func (buf Buffer) readByte() (byte, error) {
-	fmt.Println("reading byte")
-
 	b, err := buf.readBytes(1)
 	if err != nil {
 		return 0, err
 	}
-	fmt.Println("->", hex.EncodeToString(b))
 	return b[0], nil
 }
 
 func (buf Buffer) readVarUint() (uint64, error) {
-	fmt.Println("reading varuint")
-
 	var value uint64 = 0
 	var shift uint64 = 0
 
@@ -67,25 +59,18 @@ func (buf Buffer) readVarUint() (uint64, error) {
 			break
 		}
 	}
-
-	fmt.Println("->", value, "(num)")
 	return value, nil
 }
 
 func (buf Buffer) readVarBytes() ([]byte, error) {
-	fmt.Println("reading varbytes")
-
 	v, err := buf.readVarUint()
 	if err != nil {
 		return nil, err
 	}
-
 	b, err := buf.readBytes(int(v))
 	if err != nil {
 		return nil, err
 	}
-
-	fmt.Println("->", hex.EncodeToString(b))
 	return b, nil
 }
 
